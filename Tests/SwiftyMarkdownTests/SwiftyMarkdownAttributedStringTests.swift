c6afba99566719fed8d090501c1a1d1c31bffbdf
Iacopo Pazzaglia <iacopo.p.es@sync.money>
2021-04-15 15:19:12 +0200

    fix crash on text without markdown

--- Tests/SwiftyMarkdownTests/SwiftyMarkdownAttributedStringTests.swift
+++ Tests/SwiftyMarkdownTests/SwiftyMarkdownAttributedStringTests.swift
@@ -27,6 +27,8 @@ A more *complicated* example. This one has **it all**. Here is a [link](http://v
 		let md = SwiftyMarkdown(string: string)
 		let attributedString = md.attributedString()
 		
+        XCTAssertNotNil(attributedString)
+        
 		XCTAssertEqual(attributedString.string, "Heading 1\n\nA more complicated example. This one has it all. Here is a link.\n\nHeading 2\n\nHeading 3\n\nThis one is a blockquote")
 		
 		
